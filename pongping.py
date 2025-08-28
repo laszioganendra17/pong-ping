1e15b22e3018d6d779f9af6adc7c8879f367974f
laszioganendra17 <asmaraganendra@gmail.com>
2025-08-28 20:09:21 +0700

    add Gamesprite and Player class, show ball,p1 and p2

--- pongping.py
+++ pongping.py
@@ -8,8 +8,54 @@ window = display.set_mode((width, height))
 display.set_caption('Pong-Ping Not For All')
 
 
-bcakground_color = (140, 235, 108)
+bcakground_color = (92, 214, 115)
 window.fill(bcakground_color)
+
+
+class GameSprite(sprite.Sprite):
+ # class constructor
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # call for the class (Sprite) constructor:
+        sprite.Sprite.__init__(self)
+
+        # every sprite must store the image property
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        # every sprite must have the rect property that represents the rectangle it is fitted in
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    # method drawing the character on the window
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+# main player class
+class Player(GameSprite):
+   # method to control the sprite with arrow keys
+    def move_P1(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < width - 80:
+            self.rect.y += self.speed
+
+    def move_P2(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < width - 80:
+            self.rect.y += self.speed
+
+
+
+p1 = Player('racket.png', 30, 200, 30, 100, 5)
+p2 = Player('racket.png', 520, 200, 30, 100, 5)
+ball =GameSprite('tenis_ball.png', 250, 300, 50, 50, 5)
+
+clock = time.Clock()
 
 game = True
 
@@ -19,4 +65,11 @@ while game:
             game = False
 
     window.fill(bcakground_color)
+
+    p1.reset()
+    p2.reset()
+    ball.reset()
+
+    
     display.update()
+    clock.tick(60)
