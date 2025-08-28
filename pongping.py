eebbe557f628e82c62e8111c522fa877f33072ac
laszioganendra17 <asmaraganendra@gmail.com>
2025-08-28 19:42:16 +0700

    Add files via upload

--- pongping.py
+++ pongping.py
@@ -8,7 +8,7 @@ window = display.set_mode((width, height))
 display.set_caption('Pong-Ping Not For All')
 
 
-bcakground_color = (92, 214, 115)
+bcakground_color = (140, 235, 108)
 window.fill(bcakground_color)
 
 game = True
